061ee6c3c24054e55e7b9452568fdd813d34894b
Sid Agrawal <sid@sid-agrawal.ca>
2021-08-09 16:45:18 -0700

    update the walker test case

--- apps/sel4test-tests/src/tests/thread-prime.c
+++ apps/sel4test-tests/src/tests/thread-prime.c
@@ -5,24 +5,17 @@
  */
 
 #include <sel4test/test.h>
+#include <sel4test/macros.h>
 #include "../test.h"
 #include "../helpers.h"
+#include <stdio.h>
 
 #define MIN_EXPECTED_ALLOCATIONS 100
 
 int test_vspace_walker(env_t env)
 {
-    /* Perform a bunch of allocations and frees */
-    vka_object_t endpoint;
-    int error;
-
-    for (int i = 0; i < MIN_EXPECTED_ALLOCATIONS; i++) {
-        error = vka_alloc_endpoint(&env->vka, &endpoint);
-        test_error_eq(error, 0);
-        test_assert(endpoint.cptr != 0);
-        vka_free_object(&env->vka, &endpoint);
-    }
-
+    int num_res = sel4utils_walk_vspace(&env->vspace, &env->vka);
+    printf("\twalker found %d reservations\n", num_res);
     return sel4test_get_result();
 }
 DEFINE_TEST(WALKER001, "Ensure the walker works", test_vspace_walker, true)
