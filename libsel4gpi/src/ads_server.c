a6bcf9065de5e27c0f0f935441198dd9f978755c
Sid Agrawal <sid@sid-agrawal.ca>
2022-04-13 16:02:48 -0700

    Move handlers to seperate funcs

--- libsel4gpi/src/ads_server.c
+++ libsel4gpi/src/ads_server.c
@@ -97,6 +97,73 @@ static ads_server_registry_entry_t *ads_server_registry_get_entry_by_badge(seL4_
     return current_ctx;
 }
 
+static void handle_connect_req()
+{
+    printf(ADSSERVS "main: Got connect request from");
+
+    /* Allocate a new registry entry for the client. */
+    seL4_Word client_reg_ptr = (seL4_Word)malloc(sizeof(ads_server_registry_entry_t));
+    if (client_reg_ptr == 0)
+    {
+        printf(ADSSERVS "main: Failed to allocate new badge for client.\n");
+        return;
+   }
+    memset((void *)client_reg_ptr, 0, sizeof(ads_server_registry_entry_t));
+    ads_server_registry_insert((ads_server_registry_entry_t *)client_reg_ptr);
+
+    /* Create a badged endpoint for the client to send messages to.
+     * Use the address of the client_registry_entry as the badge.
+     */
+    cspacepath_t src, dest;
+    vka_cspace_make_path(get_ads_server()->server_vka,
+                         get_ads_server()->server_ep_obj.cptr, &src);
+    seL4_CPtr dest_cptr;
+    vka_cspace_alloc(get_ads_server()->server_vka, &dest_cptr);
+    vka_cspace_make_path(get_ads_server()->server_vka, dest_cptr, &dest);
+
+    int error = vka_cnode_mint(&dest, &src, seL4_AllRights, client_reg_ptr);
+    if (error)
+    {
+        printf(ADSSERVS "main: Failed to mint client badge %x.\n",
+               client_reg_ptr);
+        return;
+    }
+    /* Return this badged end point in the return message. */
+    seL4_SetCap(0, dest.capPtr);
+    seL4_SetMR(ADSMSGREG_FUNC, FUNC_CONNECT_ACK);
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(error, 0, 1, ADSMSGREG_CONNECT_ACK_END);
+    return reply(tag);
+}
+
+static void handle_attach_req(seL4_Word sender_badge)
+{
+    printf(ADSSERVS "main: Got attach request from client badge %x.\n",
+           sender_badge);
+
+    /* Find the client */
+    ads_server_registry_entry_t *client_data = ads_server_registry_get_entry_by_badge(sender_badge);
+    if (client_data == NULL)
+    {
+        printf(ADSSERVS "main: Failed to find client badge %x.\n",
+               sender_badge);
+        return;
+    }
+    printf(ADSSERVS "main: found client_data %x.\n", client_data);
+    printf(ADSSERVS "main: Doing nothing in attach for now.\n");
+
+    // void *vaddr = 0; // Get from the MSG
+    // size_t size = 0; // Get fromthe MSG
+    // error = ads_attach(&client_data->ads, get_ads_server()->server_vka, vaddr, size);
+    // if (error) {
+    //     printf(ADSSERVS "main: Failed to attach at vaddr:%lx sz: %lx to client badge %x.\n",
+    //             vaddr, size, sender_badge);
+    //     continue;
+    // }
+
+    seL4_SetMR(ADSMSGREG_FUNC, FUNC_ATTACH_ACK);
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, ADSMSGREG_ATTACH_ACK_END);
+    return reply(tag);
+}
 /**
  * @brief The starting point for the ads server's thread.
  *
@@ -106,7 +173,6 @@ void ads_server_main()
     seL4_MessageInfo_t tag;
     enum ads_server_funcs func;
     seL4_Error error = 0;
-    ads_server_registry_entry_t *client_data = NULL;
     size_t buff_len, bytes_written;
 
     /* The Parent will seL4_Call() to us, the Server, right after spawning us.
@@ -151,67 +217,11 @@ void ads_server_main()
 
         switch (func) {
         case FUNC_CONNECT_REQ:
-            printf(ADSSERVS"main: Got connect request from client badge %x.\n",
-                    sender_badge);
-
-            /* Allocate a new registry entry for the client. */
-            seL4_Word client_reg_ptr = (seL4_Word) malloc(sizeof(ads_server_registry_entry_t));
-            if (client_reg_ptr == 0) {
-                printf(ADSSERVS "main: Failed to allocate new badge for client.\n");
-                continue;
-            }
-            memset((void *) client_reg_ptr, 0, sizeof(ads_server_registry_entry_t));
-            ads_server_registry_insert((ads_server_registry_entry_t *) client_reg_ptr);
-
-            /* Create a badged endpoint for the client to send messages to.
-             * Use the address of the client_registry_entry as the badge.
-             */ 
-            cspacepath_t src, dest;
-            vka_cspace_make_path(get_ads_server()->server_vka,
-                                get_ads_server()->server_ep_obj.cptr, &src);
-            seL4_CPtr dest_cptr;
-            vka_cspace_alloc(get_ads_server()->server_vka, &dest_cptr);
-            vka_cspace_make_path(get_ads_server()->server_vka, dest_cptr, &dest);
-
-            error = vka_cnode_mint(&dest, &src, seL4_AllRights, client_reg_ptr);
-            if (error) {
-                printf(ADSSERVS "main: Failed to mint client badge %x.\n",
-                        client_reg_ptr);
-                continue;
-            }
-            /* Return this badged end point in the return message. */
-            seL4_SetCap(0, dest.capPtr);
-            seL4_SetMR(ADSMSGREG_FUNC, FUNC_CONNECT_ACK);
-            tag = seL4_MessageInfo_new(error, 0, 1, ADSMSGREG_CONNECT_ACK_END);
-            reply(tag);
+            handle_connect_req();
             break;
 
         case FUNC_ATTACH_REQ:
-            printf(ADSSERVS"main: Got attach request from client badge %x.\n",
-                    sender_badge);
-
-            /* Find the client */
-            client_data = ads_server_registry_get_entry_by_badge(sender_badge);
-            if (client_data == NULL) {
-                printf(ADSSERVS "main: Failed to find client badge %x.\n",
-                        sender_badge);
-                continue;
-            }
-            printf(ADSSERVS"main: found client_data %x.\n", client_data);
-            printf(ADSSERVS"main: Doing nothing in attach for now.\n");
-
-            // void *vaddr = 0; // Get from the MSG
-            // size_t size = 0; // Get fromthe MSG
-            // error = ads_attach(&client_data->ads, get_ads_server()->server_vka, vaddr, size);
-            // if (error) {
-            //     printf(ADSSERVS "main: Failed to attach at vaddr:%lx sz: %lx to client badge %x.\n",
-            //             vaddr, size, sender_badge);
-            //     continue;
-            // }
-
-            seL4_SetMR(ADSMSGREG_FUNC, FUNC_ATTACH_ACK);
-            tag = seL4_MessageInfo_new(error, 0, 0, ADSMSGREG_ATTACH_ACK_END);
-            reply(tag);
+            handle_attach_req(sender_badge);
             break;
 
 
