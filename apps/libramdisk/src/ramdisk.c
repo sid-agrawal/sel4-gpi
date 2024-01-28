850767f0179ab4f6a5f34c42ff6a26639acae1f4
Arya Stevinson <arya.stevinson@gmail.com>
2024-01-28 14:39:02 -0800

    Implement basic ramdisk

--- apps/libramdisk/src/ramdisk.c
+++ apps/libramdisk/src/ramdisk.c
@@ -14,6 +14,19 @@
 
 #include <ramdisk/ramdisk.h>
 
+/* Memory regions for IPC to ramdisk server */
+#define RAMDISK_OP 0
+#define RAMDISK_SECTOR 1
+
+/* Ramdisk opcodes */
+#define RAMDISK_READ 0
+#define RAMDISK_WRITE 1
+#define RAMDISK_FLUSH 2
+#define RAMDISK_REGISTER_CLIENT 3
+
+/* Other constants */
+#define IPC_FRAME_PAGE_BITS seL4_PageBits
+
 #define RAMDISK_PRINTF(...)       \
     do                            \
     {                             \
@@ -21,6 +34,21 @@
         printf(__VA_ARGS__);      \
     } while (0);
 
+#define CHECK_ERROR(error, msg)        \
+    do                                 \
+    {                                  \
+        if (error != seL4_NoError)     \
+        {                              \
+            ZF_LOGE(RAMDISK_S "%s: %s" \
+                              ", %d.", \
+                    __func__,          \
+                    msg,               \
+                    error);            \
+            return error;              \
+        }                              \
+    } while (0);
+
+/*--- RAMDISK SERVER ---*/
 static ramdisk_server_context_t ramdisk_server;
 
 ramdisk_server_context_t *get_ramdisk_server(void)
@@ -64,13 +92,14 @@ ramdisk_server_spawn_thread(simple_t *parent_simple, vka_t *parent_vka,
     get_ramdisk_server()->server_vka = parent_vka;
     get_ramdisk_server()->server_cspace = parent_cspace_cspath.root;
     get_ramdisk_server()->server_vspace = parent_vspace;
+    get_ramdisk_server()->shared_mem =  NULL;
 
     /* Get a CPtr to the parent's root cnode. */
     vka_cspace_make_path(parent_vka, 0, &parent_cspace_cspath);
 
     /* Allocate the Endpoint that the server will be listening on. */
     error = vka_alloc_endpoint(parent_vka, &get_ramdisk_server()->server_ep_obj);
-    if (error != 0)
+    if (error != seL4_NoError)
     {
         ZF_LOGE(RAMDISK_S "spawn_thread: failed to alloc endpoint, err=%d.",
                 error);
@@ -83,7 +112,7 @@ ramdisk_server_spawn_thread(simple_t *parent_simple, vka_t *parent_vka,
 
     /*
     error = vka_alloc_frame(parent_vka, RAMDISK_SIZE_BITS, &get_ramdisk_server()->ramdisk_buf_obj);
-    if (error)
+    if (error != seL4_NoError)
     {
         ZF_LOGE(RAMDISK_S "spawn_thread: failed to alloc virtual disk, err=%d.",
                 error);
@@ -131,7 +160,7 @@ ramdisk_server_spawn_thread(simple_t *parent_simple, vka_t *parent_vka,
                                               parent_vspace,
                                               config,
                                               &get_ramdisk_server()->server_thread);
-    if (error != 0)
+    if (error != seL4_NoError)
     {
         ZF_LOGE(RAMDISK_S "spawn_thread: sel4utils_configure_thread failed "
                           "with %d.",
@@ -143,7 +172,7 @@ ramdisk_server_spawn_thread(simple_t *parent_simple, vka_t *parent_vka,
     error = sel4utils_start_thread(&get_ramdisk_server()->server_thread,
                                    (sel4utils_thread_entry_fn)&ramdisk_server_main,
                                    NULL, NULL, 1);
-    if (error != 0)
+    if (error != seL4_NoError)
     {
         ZF_LOGE(RAMDISK_S "spawn_thread: sel4utils_start_thread failed with "
                           "%d.",
@@ -155,9 +184,37 @@ ramdisk_server_spawn_thread(simple_t *parent_simple, vka_t *parent_vka,
 
 out:
     RAMDISK_PRINTF("spawn_thread: Server ran into an error.\n");
-    vka_free_object(parent_vka, &get_ramdisk_server()->server_ep_obj);
+    vka_free_object(parent_vka, &get_ramdisk_server()->server_ep_obj); // ARYA-TODO does this unmap?
     vka_free_object(parent_vka, &get_ramdisk_server()->ramdisk_buf_obj);
     return error;
+}
+
+/**
+ * Map a shared frame in the vspace
+ */
+static seL4_Error map_shared_frame(vspace_t *vspace, seL4_CPtr *frame, void **vaddr)
+{
+    *vaddr = vspace_map_pages(vspace, frame, NULL, seL4_ReadWrite, 1, seL4_PageBits, 0);
+
+    return vaddr == NULL; // Returns error if vaddr is NULL
+}
+
+/**
+ * Unmap a shared frame from the vspace
+ */
+static void unmap_shared_frame(vspace_t *vspace,  void *vaddr)
+{
+    // ARYA-TODO free the shared mem cap as well
+    vspace_unmap_pages(vspace, vaddr, 1, seL4_PageBits, NULL);
+}
+
+/**
+ * Create a pointer to the ramdisk buf from a sector and offset
+ */
+static void *ramdisk_ptr(unsigned int sector)
+{
+    assert(sector < (RAMDISK_SIZE_BYTES / RAMDISK_BLOCK_SIZE));
+    return get_ramdisk_server()->ramdisk_buf + sector * RAMDISK_BLOCK_SIZE;
 }
 
 /**
@@ -173,28 +230,175 @@ void ramdisk_server_main()
     seL4_Word sender_badge;
     cspacepath_t received_cap_path;
 
-    while (1)
+    // Allocate initial cap receive path
+    error = vka_cspace_alloc_path(get_ramdisk_server()->server_vka, &received_cap_path);
+    if (error != seL4_NoError)
     {
-        /* Alloc slot for frame cap from the message */
-        error = vka_cspace_alloc_path(get_ramdisk_server()->server_vka, &received_cap_path);
-        assert(error == 0);
+        ZF_LOGE(RAMDISK_S "%s: failed to alloc initial cap receive path ",
+                __func__);
+        goto exit;
+    }
 
-        seL4_SetCapReceivePath(
+    seL4_SetCapReceivePath(
             /* _service */ received_cap_path.root,
             /* index */ received_cap_path.capPtr,
             /* depth */ received_cap_path.capDepth);
 
+    while (1)
+    {
         tag = recv(&sender_badge);
+        unsigned int op = seL4_GetMR(RAMDISK_OP);
 
-        seL4_SetMR(0, 42);
-        seL4_MessageInfo_t reply_tag = seL4_MessageInfo_new(error, 0, 0, 1);
+        seL4_MessageInfo_t reply_tag;
 
-        // Send an empty reply, for now
+        switch (op)
+        {
+        case RAMDISK_REGISTER_CLIENT:
+            // Map the shared memory page
+            assert(seL4_MessageInfo_get_extraCaps(tag) == 1);
+            map_shared_frame(get_ramdisk_server()->server_vspace, &received_cap_path.capPtr, &get_ramdisk_server()->shared_mem);
+
+            /* Alloc slot for future IPC caps */
+            // ARYA-TODO need to free this slot?
+            error = vka_cspace_alloc_path(get_ramdisk_server()->server_vka, &received_cap_path);
+            assert(error == 0);
+
+            seL4_SetCapReceivePath(
+                /* _service */ received_cap_path.root,
+                /* index */ received_cap_path.capPtr,
+                /* depth */ received_cap_path.capDepth);
+            
+            break;
+        case RAMDISK_READ:
+        case RAMDISK_WRITE:
+            if (&get_ramdisk_server()->shared_mem == NULL) {
+                ZF_LOGE(RAMDISK_S "%s: client hasn't registered shared memory ",
+                        __func__);
+                error = 1;
+                goto done;
+            }
+
+            void *ramdisk_vaddr = ramdisk_ptr(seL4_GetMR(RAMDISK_SECTOR));
+
+            if (op == RAMDISK_READ)
+            {
+                memcpy(get_ramdisk_server()->shared_mem, ramdisk_vaddr, RAMDISK_BLOCK_SIZE);
+            }
+            else
+            { // op is write
+                memcpy(ramdisk_vaddr, get_ramdisk_server()->shared_mem, RAMDISK_BLOCK_SIZE);
+            }
+            break;
+        case RAMDISK_FLUSH:
+            // ARYA-TODO what to do for flush?
+            error = 0;
+            break;
+        default:
+            ZF_LOGE(RAMDISK_S "%s: got unexpected opcode %d\n",
+                    __func__,
+                    op);
+
+            error = 1;
+        }
+
+    done:
+        reply_tag = seL4_MessageInfo_new(error, 0, 0, 1);
         reply(reply_tag);
     }
 
     // serial_server_func_kill();
     /* After we break out of the loop, seL4_TCB_Suspend ourselves */
+exit:
     ZF_LOGI(RAMDISK_S "main: Suspending.");
     seL4_TCB_Suspend(get_ramdisk_server()->server_thread.tcb.cptr);
+}
+
+/*--- RAMDISK CLIENT ---*/
+
+static ramdisk_client_context_t ramdisk_client;
+
+ramdisk_client_context_t *get_ramdisk_client(void)
+{
+    return &ramdisk_client;
+}
+
+seL4_Error
+ramdisk_client_init(vka_t *client_vka,
+                    vspace_t *client_vspace,
+                    seL4_CPtr server_ep_cap)
+{
+    get_ramdisk_client()->client_vka = client_vka;
+    get_ramdisk_client()->client_vspace = client_vspace;
+    get_ramdisk_client()->server_ep_cap = server_ep_cap;
+
+    /* Alloc shared memory for IPC */
+    seL4_Error error;
+    vka_object_t frame_obj;
+    error = vka_alloc_frame(get_ramdisk_client()->client_vka, seL4_PageBits, &frame_obj);
+    CHECK_ERROR(error, "failed to allocate shared memory\n");
+
+    error = map_shared_frame(get_ramdisk_client()->client_vspace, &frame_obj.cptr, &get_ramdisk_client()->shared_mem);
+    CHECK_ERROR(error, "failed to map shared memory\n");
+    // ARYA-TODO: support deregistering client and unmapping these frames
+
+    /* Register shared mem with the server */
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 1, 1);
+    seL4_SetMR(RAMDISK_OP, RAMDISK_REGISTER_CLIENT);
+    seL4_SetCap(0, frame_obj.cptr);
+    tag = seL4_Call(get_ramdisk_client()->server_ep_cap, tag);
+    error = seL4_MessageInfo_get_label(tag);
+    CHECK_ERROR(error, "failed to register client with ramdisk server\n");
+
+    return error;
+}
+
+int ramdisk_read(unsigned int sector, void *buf)
+{
+    seL4_Error error;
+
+    // Send IPC to ramdisk server
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 2);
+    seL4_SetMR(RAMDISK_OP, RAMDISK_READ);
+    seL4_SetMR(RAMDISK_SECTOR, sector);
+    tag = seL4_Call(get_ramdisk_client()->server_ep_cap, tag);
+    error = seL4_MessageInfo_get_label(tag);
+
+    // Copy result to local buffer
+    if (error == seL4_NoError)
+    {
+        memcpy(buf, get_ramdisk_client()->shared_mem, RAMDISK_BLOCK_SIZE);
+    }
+
+    return error;
+}
+
+int ramdisk_write(unsigned int sector, void *buf)
+{
+    seL4_Error error;
+
+    // Copy buffer to ipc frame
+    memcpy(get_ramdisk_client()->shared_mem, buf, RAMDISK_BLOCK_SIZE);
+
+    // Send IPC to ramdisk server
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 2);
+    seL4_SetMR(RAMDISK_OP, RAMDISK_WRITE);
+    seL4_SetMR(RAMDISK_SECTOR, sector);
+    tag = seL4_Call(get_ramdisk_client()->server_ep_cap, tag);
+    error = seL4_MessageInfo_get_label(tag);
+
+    return error;
+}
+
+int ramdisk_flush(void)
+{
+    seL4_Error error;
+
+    // Send IPC to ramdisk server
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
+    seL4_SetMR(RAMDISK_OP, RAMDISK_FLUSH);
+
+    tag = seL4_Call(get_ramdisk_client()->server_ep_cap, tag);
+    error = seL4_MessageInfo_get_label(tag);
+
+    return error;
 }
--- apps/sel4test-tests/src/tests/ramdisk.c
+++ apps/sel4test-tests/src/tests/ramdisk.c
@@ -12,18 +12,57 @@
 
 #include <sel4gpi/pd_clientapi.h>
 #include <sel4bench/arch/sel4bench.h>
+#include <ramdisk/ramdisk.h>
+
+#define TEST_STR_1 "Fuzzy Wuzzy was a bear"
+#define TEST_STR_2 "Fuzzy Wuzzy had no hair"
 
 int test_ramdisk(env_t env)
 {
-    int error;
+    seL4_Error error;
+    char* buf = malloc(RAMDISK_BLOCK_SIZE);
+
     printf("------------------STARTING: %s------------------\n", __func__);
 
-    // Check that the ramdisk is running
-    seL4_CPtr ramdisk_ep = env->ramdisk_endpoint;
-    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 0);
-    tag = seL4_Call(ramdisk_ep, tag);
+    // Should get failure before initializing client
+    error = ramdisk_read(0, buf);
+    assert(error != seL4_NoError);
 
-    assert(seL4_GetMR(0) == 42);
+    // Initialize the ramdisk client
+    ramdisk_client_init(&env->vka, &env->vspace, env->ramdisk_endpoint);
+
+    // Write and read from beginning of disk
+    strcpy(buf, TEST_STR_1);
+    error = ramdisk_write(0, buf);
+    test_assert(error == seL4_NoError);
+
+    memset(buf, 0, RAMDISK_BLOCK_SIZE); // clear the test string from the buffer
+    error = ramdisk_read(0, buf);
+    test_assert(error == seL4_NoError);
+    test_assert(strcmp(buf, TEST_STR_1) == 0);
+
+    // Write and read from offset into disk
+    unsigned int sector = 10;
+    strcpy(buf, TEST_STR_2);
+    error = ramdisk_write(sector, buf);
+    test_assert(error == seL4_NoError);
+
+    memset(buf, 0, RAMDISK_BLOCK_SIZE); // clear the test string from the buffer
+    error = ramdisk_read(sector, buf);
+    test_assert(error == seL4_NoError);
+    test_assert(strcmp(buf, TEST_STR_2) == 0);
+
+    // Write/read entire block
+    memset(buf, 0x42, RAMDISK_BLOCK_SIZE);
+
+    error = ramdisk_write(0, buf);
+    test_assert(error == seL4_NoError);
+
+    memset(buf, 0, RAMDISK_BLOCK_SIZE); // clear the test string from the buffer
+    error = ramdisk_read(0, buf);
+    test_assert(error == seL4_NoError);
+    test_assert(buf[0] == 0x42);
+    test_assert(buf[RAMDISK_BLOCK_SIZE - 1] == 0x42);
     
     printf("------------------ENDING: %s------------------\n", __func__);
     return sel4test_get_result();
